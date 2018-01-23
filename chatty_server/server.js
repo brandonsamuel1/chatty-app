fa0074b5f8014fc6be9d8c3a950b3a7cb4c2feab
Brandon Samuel <brandonsamuel72@gmail.com>
2018-01-23 22:54:59 +0000

    added chatty server

---
+++ chatty_server/server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const WebSocket = require('ws');
+const SocketServer = WebSocket.Server;
+const uuid = require('uuid/v1');
+
+// Set the port to 3001
+const PORT = 3001;
+
+// Create a new express server
+const server = express()
+   // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+// Create the WebSockets server
+const wss = new SocketServer({ server });
+
+// Set up a callback that will run when a client connects to the server
+// When a client connects they are assigned a socket, represented by
+// the ws parameter in the callback.
+wss.broadcast = (data) => {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  });
+};
+
+const clients = [];
+
+wss.countUser = function countUser(data) {
+  const usersNumber = {
+    type: 'newUser',
+    count: data
+  }
+  return usersNumber;
+}
+
+
+
+wss.on('connection', (ws) => {
+  console.log('New client connected');
+  clients.push(ws);
+  let userCount = wss.countUser(wss.clients.size);
+  console.log('user count:', userCount)
+  wss.broadcast(userCount);
+
+  ws.on('message', (data) => {
+    const message = JSON.parse(data);
+    message.id = uuid();
+    console.log('message received:', message);
+    wss.broadcast(message);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected')
+    const userCount = wss.countUser(wss.clients.size);
+    wss.broadcast(userCount);
+  });
+})
+
+
+   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+
+
